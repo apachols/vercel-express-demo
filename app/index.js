28dc902574afcd36e350c701f841d5d7a6a1e39e
Adam Pacholski <apachols@gmail.com>
2020-12-31 11:36:52 -0800

    Try to add post route

--- app/index.js
+++ app/index.js
@@ -1,4 +1,6 @@
 const monk = require('monk');
+const yup = require('yup');
+const { nanoid } = require('nanoid');
 
 const db = monk(process.env.MONGODB_URI);
 const urls = db.get('urls');
@@ -13,9 +15,45 @@ app.get('/api', (req, res) => {
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 });
 
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+const schema = yup.object().shape({
+  slug: yup
+    .string()
+    .trim()
+    .matches(/[\w\-]/i),
+  url: yup.string().trim().url().required()
+});
+
+app.post('/api/url', async (req, res, next) => {
+  let { slug, url } = req.body;
+  try {
+    if (!slug) {
+      await schema.validate({
+        url
+      });
+    } else {
+      await schema.validate({
+        slug,
+        url
+      });
+    }
+
+    if (!slug) {
+      slug = nanoid(7);
+    }
+    slug = slug.toLowerCase();
+
+    const newUrl = {
+      url,
+      slug
+    };
+    const created = await urls.insert(newUrl);
+    res.json(created);
+  } catch (error) {
+    if (error.message.startsWith('E11000')) {
+      error.message = 'Slug in use. 🐛';
+    }
+    next(error);
+  }
 });
 
 app.get('/api/:id', async (req, res) => {
@@ -25,11 +63,13 @@ app.get('/api/:id', async (req, res) => {
     if (url) {
       res.statusCode = 302;
       res.setHeader('Location', url.url);
+      // Make sure to explicitly send a response body with the redirect
+      // Otherwise, vercel thinks the response was empty and 502s
       res.end('redirect with a body');
     }
-    res.end(`/?error=${slug} not found`);
+    res.end(`Error: ${slug} not found`);
   } catch (error) {
-    res.end(`/?error=Link not found`);
+    res.end(`Error: ${error}`);
   }
 });
 
