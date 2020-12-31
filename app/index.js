14578e05e08d15dc74000809b1210e2c8165e6c0
Adam Pacholski <apachols@gmail.com>
2020-12-31 11:08:36 -0800

    Add mongodb connection

--- app/index.js
+++ app/index.js
@@ -1,7 +1,13 @@
+const monk = require('monk');
+
+const db = monk(process.env.MONGODB_URI);
+const urls = db.get('urls');
+urls.createIndex({ url: 1, slug: 1 }, { unique: true });
+
 const app = require('express')();
 
 app.get('/api', (req, res) => {
-  const path = `/api/item/hello-world/${process.env.MONGODB_URI}`;
+  const path = `/api/item/hello-world/`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
@@ -10,6 +16,19 @@ app.get('/api', (req, res) => {
 app.get('/api/item/:slug', (req, res) => {
   const { slug } = req.params;
   res.end(`Item: ${slug}`);
+});
+
+app.get('/api/:id', async (req, res) => {
+  const { id: slug } = req.params;
+  try {
+    const url = await urls.findOne({ slug });
+    if (url) {
+      res.redirect(url.url);
+    }
+    res.redirect(`/?error=${slug} not found`);
+  } catch (error) {
+    res.redirect(`/?error=Link not found`);
+  }
 });
 
 module.exports = app;
